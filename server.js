a9fadccf1757308521bc7056f3b7311c2e538851
Tony Andrade <aandrade0205@gmail.com>
2020-09-07 12:01:53 -0500

    Formatting

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 //dependencies
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 //db configs
-const db = require('./configs/keys').mongoURI;
+const db = require("./configs/keys").mongoURI;
 
 //Connection to mongodb
-mongoose.connect(db).then(() => console.log('Succesfully connected to db!'));
+mongoose
+  .connect(db)
+  .then(() => console.log("Succesfully connected to db!"))
+  .catch((err) => console.log(err));
 //vars
 const app = express();
 const port = process.env.PORT || 5000;
 
 //initial route
-app.get('/', (req, res) => {
-  res.send('You made it!')
+app.get("/", (req, res) => {
+  res.send("You made it!");
 });
 
 //server start listening
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-})
+});
